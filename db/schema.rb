a5d78f0527d7f615cc922247ba5e2e0dd9239939
Daiki Kinoshita <58548429+Daiki-knst@users.noreply.github.com>
2020-07-12 16:04:30 +0900

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_063346) do
+ActiveRecord::Schema.define(version: 2020_07_12_070124) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
